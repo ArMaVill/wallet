f0ad1597f7cb174446274a6336b5db2d8d41b4b0
ArMaVill <49162683+ArMaVill@users.noreply.github.com>
2019-08-26 18:38:28 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { IncomeComponent } from "./income/income.component";
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { DailyComponent } from "./daily/daily.component";
+
+import { ExpenseComponent } from "./expense/expense.component";
+import { AccountComponent } from "./account/account.component";
+import { AccountListComponent } from "./account-list/account-list.component";
+import { IncomeListComponent } from "./income/income-list.component";
+import { ExpenseListComponent } from "./expense/expense-list.component";
+import { TransactionComponent } from './transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    IncomeComponent,
+    DashboardComponent,
+    DailyComponent,
+    ExpenseComponent,
+    AccountComponent,
+    AccountListComponent,
+    IncomeListComponent,
+    ExpenseListComponent,
+    TransactionComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
