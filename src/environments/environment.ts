9f6b8abf72eb6aa7420c0c983061f6c4809ae964
Arturo Madrigal <arturo.madrigalv@gmail.com>
2019-10-14 17:20:34 -0600

    preparing for build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://wallet-rest.herokuapp.com//api'
 };
 
 /*
