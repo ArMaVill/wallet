c54e7f02669a90eec9420e3eef6d6637ae36db1f
Arturo Madrigal <arturo.madrigalv@gmail.com>
2019-10-14 18:54:02 -0600

    Building app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://wallet-rest.herokuapp.com//api'
+  apiUrl: 'https://wallet-rest.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://wallet-rest.herokuapp.com//api'
+  apiUrl: 'https://wallet-rest.herokuapp.com/api'
 };
 
 /*
